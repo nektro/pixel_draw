cc01c5883fb912ec83957a5aa56352848e8706d4
SamuelDeboni <samueldeboniss@gmail.com>
2020-11-16 20:03:51 -0300

    Separeted pixel draw int its own module to allow custom windowing system

--- src/main.zig
+++ src/main.zig
@@ -9,11 +9,13 @@ const draw = @import("pixel_draw.zig");
 usingnamespace draw.vector_math;
 const Texture = draw.Texture;
 
-// === Global Variables ===
+// ========== Global Variables =============
 var main_allocator: *Allocator = undefined;
 var font: draw.BitmapFont = undefined;
 var potato: Texture = undefined;
-var bad_floor: Texture = undefined;
+var earth_mesh: draw.Mesh = undefined;
+var mundi: draw.Texture = undefined;
+// =========================================
 
 pub fn main() anyerror!void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -25,11 +27,6 @@ pub fn main() anyerror!void {
     end();
 }
 
-var quad_mesh: draw.Mesh = undefined;
-var mesh: draw.Mesh = undefined;
-var palheta: draw.Texture = undefined;
-var mundi: draw.Texture = undefined;
-
 fn start() void {
     font = .{
         .texture = draw.textureFromTgaData(main_allocator, @embedFile("../assets/font.tga")) catch unreachable,
@@ -37,98 +34,32 @@ fn start() void {
         .font_size_y = 16,
         .character_spacing = 11,
     };
+    
     potato = draw.textureFromTgaData(main_allocator, @embedFile("../assets/potato.tga")) catch unreachable;
     mundi = draw.textureFromTgaData(main_allocator, @embedFile("../assets/mundi.tga")) catch unreachable;
-    bad_floor = draw.textureFromTgaData(main_allocator, @embedFile("../assets/bad_floor.tga")) catch unreachable;
     
-    cube_mesh.texture = potato;
-    quad_mesh = draw.createQuadMesh(main_allocator, 32, 32, 0, 0, mundi, .Strech);
-    
-    for (quad_mesh.v) |*v| {
-        //v.pos.y -= 0.0;
+    earth_mesh = draw.createQuadMesh(main_allocator, 32, 32, 0, 0, mundi, .Strech);
+    for (earth_mesh.v) |*v| {
         v.pos.x = (v.pos.x / 32.0) * -3.141592 * 2;
         v.pos.y = (v.pos.y / 32.0) * -3.141592;
         v.pos = sphericalToCartesian(10.0, v.pos.y, v.pos.x);
         v.pos = rotateVectorOnX(v.pos, -3.1415926535 * 0.5);
     }
-    
-    sky = draw.meshFromObjData(main_allocator, @embedFile("../assets/SkySphere.obj"));
-    clock = draw.meshFromObjData(main_allocator, @embedFile("../assets/GreatGeorge.obj"));
-    terrain = draw.meshFromObjData(main_allocator, @embedFile("../assets/cenario.obj"));
-    mesh= draw.meshFromObjData(main_allocator, @embedFile("../assets/lampada_mapeada.obj"));
-    mesh.texture = potato;
-    
-    palheta = draw.textureFromTgaData(main_allocator, @embedFile("../assets/Palheta2.tga")) catch unreachable;
-    //sky.texture = draw.textureFromTgaData(main_allocator, @embedFile("../assets/sky.tga")) catch unreachable;
-    sky.texture = mundi;
-    
-    for (clock.v) |*v| {
-        v.pos.y -= 0.25;
-    }
-    
-    for (sky.v) |*v| {
-        v.pos = Vec3_mul_F(v.pos, 500);
-    }
-    
-    clock.texture = palheta;
-    terrain.texture = palheta;
-    //quad_mesh.texture = palheta;
 }
-
-var clock: draw.Mesh = undefined;
-var sky: draw.Mesh = undefined;
-var terrain: draw.Mesh = undefined;
 
 fn end() void {
     main_allocator.free(font.texture.raw);
     main_allocator.free(potato.raw);
-    main_allocator.free(bad_floor.raw);
-    main_allocator.free(quad_mesh.v);
-    main_allocator.free(quad_mesh.i);
+    main_allocator.free(mundi.raw);
+    main_allocator.free(earth_mesh.v);
+    main_allocator.free(earth_mesh.i);
 }
 
-var print_buff: [512]u8 = undefined;
-
-var cube_v = [_]Vertex{
-    Vertex.c(Vec3.c(-0.5, 0.5, 0.5), Vec2.c(0, 1)),
-    Vertex.c(Vec3.c(0.5, 0.5, 0.5), Vec2.c(1, 1)),
-    Vertex.c(Vec3.c(-0.5, -0.5, 0.5), Vec2.c(0, 0)),
-    Vertex.c(Vec3.c(0.5, -0.5, 0.5), Vec2.c(1, 0)),
-    
-    Vertex.c(Vec3.c(-0.5, 0.5, -0.5), Vec2.c(1, 1)),
-    Vertex.c(Vec3.c(0.5, 0.5, -0.5), Vec2.c(0, 1)),
-    Vertex.c(Vec3.c(-0.5, -0.5, -0.5), Vec2.c(1, 0)),
-    Vertex.c(Vec3.c(0.5, -0.5, -0.5), Vec2.c(0, 0)),
-};
-
-var cube_i = [_]u32{
-    0, 2, 3,
-    0, 3, 1,
-    1, 3, 7,
-    1, 7, 5,
-    4, 0, 1,
-    4, 1, 5,
-    4, 6, 2,
-    4, 2, 0,
-    6, 3, 2,
-    6, 7, 3,
-    5, 7, 6,
-    5, 6, 4,
-};
-
-var cube_mesh = draw.Mesh{
-    .v = &cube_v,
-    .i = &cube_i,
-    .texture = undefined,
-};
-
+var cam: draw.Camera3D = .{ .pos = .{ .z = 2.0 }, .far = 100000 };
 var mov_speed: f32 = 2.0;
 
 fn update(delta: f32) void {
     draw.fillScreenWithRGBColor(0, 0, 0);
-    //draw.fillScreenWithRGBColor(50, 100, 150);
-    
-    // NOTE(Samuel): Mesh Transformation
     
     if (draw.keyPressed(.up)) cam.rotation.x += delta * 2;
     if (draw.keyPressed(.down)) cam.rotation.x -= delta * 2;
@@ -158,21 +89,8 @@ fn update(delta: f32) void {
     
     cam.pos = Vec3_add(camera_delta_p, cam.pos);
     
-    //draw.drawMesh(terrain, .Texture, cam);
-    //draw.drawMesh(terrain, .Lines, cam);
+    draw.drawMesh(earth_mesh, .Texture, cam);
     
-    //draw.drawMesh(clock, .Texture, cam);
-    //draw.drawMesh(clock, .Lines, cam);
-    
-    //draw.drawMesh(sky, .Texture, cam);
-    
-    draw.drawMesh(quad_mesh, .Texture, cam);
-    //draw.drawMesh(mesh, .Texture, cam);
-    
-    { // Show fps
-        const fpst = std.fmt.bufPrint(&print_buff, "{d:0.4}/{d:0.4}/{d}", .{ 1 / delta, delta, mov_speed }) catch unreachable;
-        draw.drawBitmapFont(fpst, 20, 20, 1, 1, font);
-    }
+    draw.drawBitmapFontFmt("{d:0.4}/{d:0.4}/{d}", .{ 1 / delta, delta, mov_speed }, 20, 20, 1, 1, font);
 }
 
-var cam: draw.Camera3D = .{ .pos = .{ .z = 2.0 }, .far = 100000 };
